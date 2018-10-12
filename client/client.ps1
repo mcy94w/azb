8d39585636aaffa5e75015aee63add9e1598755a
Chiyuan Ma <chiyma@microsoft.com>
2018-10-12 11:32:50 -0700

    update

---
+++ client/client.ps1
@@ -0,0 +1,12 @@
+Param(
+  [string]$server="https://www.google.com"
+)
+
+# $resp = Invoke-WebRequest -UseBasicParsing -Uri $server
+
+# if ($resp -eq 200) {
+#     Write-Host "200"
+# }
+
+# sleep 3
+exit 1
